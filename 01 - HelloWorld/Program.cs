b47f1f53cf04806900f4bddf03386443c173d2a8
Rodolfo Fortes <rfortes@gmail.com>
2019-02-20 00:17:19 -0300

    first commit

---
+++ 01 - HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+        }
+    }
+}
